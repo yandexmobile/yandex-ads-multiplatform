3e8b441fa71f19aae0501bdcd37b2880dc31e349
mobile-ads-github <mobile-ads-github@yandex-team.ru>
2026-09-03 16:47:49 +0300

    Release 8.4.0

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -29,6 +29,7 @@ kotlin {
         androidMain.dependencies {
             implementation(libs.compose.uiToolingPreview)
             implementation(libs.androidx.activity.compose)
+            implementation(libs.mobileads.mediation)
         }
         commonMain.dependencies {
             implementation(libs.mobileads.compose)
